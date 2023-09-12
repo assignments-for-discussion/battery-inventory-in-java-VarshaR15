1f0b78b110c6d6fc8665c70779b0c2aad00bd647
VarshaR15 <133860947+VarshaR15@users.noreply.github.com>
2023-09-12 12:51:31 +0530

    Update Main.java

--- bunchbysoh/Main.java
+++ bunchbysoh/Main.java
@@ -9,6 +9,17 @@ public class Main {
 
   static CountsBySoH countBatteriesByHealth(int[] presentCapacities) {
     CountsBySoH counts = new CountsBySoH();
+    for (int i = 0; i < presentCapacities.length; i++) {
+            double sohPercentage = (presentCapacities[i] / ratedCapacity) * 100.0;
+
+            if (sohPercentage >= 90.0) {
+                counts.healthy++;
+            } else if (sohPercentage >= 70.0) {
+                counts.exchange++;
+            } else {
+                counts.failed++;
+            }
+        }
     return counts;
   }
 
